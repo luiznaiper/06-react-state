d687ae33edb369ed5b0a220910c27577d6d37df8
luiznaiper <luiz_naiper@hotmail.com>
2022-05-04 07:15:33 -0300

    Added useEffect to function component, loading

--- src/UseState.js
+++ src/UseState.js
@@ -2,6 +2,25 @@ import React from "react";
 
 function UseState({ name }){
     const [error, setError] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+
+    React.useEffect(() => {
+      console.log('Starting effect')
+
+        if(loading){
+            setTimeout(()=> { 
+                console.log('Validating setTimeout')
+    
+                setLoading(false)
+                
+                console.log('Endind setTimeout')
+            }, 3000)
+        }
+
+      console.log('Ending effect')
+    
+    },[loading])
+    
 
     return (
         <div>
@@ -11,10 +30,13 @@ function UseState({ name }){
                 { error && (
                     <p>Error: your code is wrong</p>
                 ) }
+                { loading &&(
+                    <p>Loading...</p>
+                )}
 
                 <input placeholder="Security Code"/>
                 <button
-                    onClick={() => setError(!error)}
+                    onClick={() => setLoading(true)}
                 >
                     Submit
                 </button>
